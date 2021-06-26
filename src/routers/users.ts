7c11b6587ce172fa1751d294e991360791bb25a5
mobileguru007 <zhezui727@gmail.com>
2021-06-26 20:32:00 +0100

    update api

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -9,14 +9,40 @@ router.post("/users/login", async (req: Request, res: Response) => {
       const user = await User.findByCredentials(email, password);
       const userObject = transformUser(user);
 
-      res.send({ user: userObject });
+      res.send({ 
+        success: true,
+        user: userObject 
+      });
     } catch (error) {
-      console.log(error);
       res.status(400).send({
-        error: {
-          message: "Email or password is not correct",
-        },
+        success: false,
+        error: error.message
       });
     }
   });
+
+router.post("/users/register", async (req: Request, res: Response) => {
+  try {
+    const {
+      email,
+      password,
+      name
+    } = req.body;
+    const user = new User({
+      email,
+      password,
+      name,
+    });
+    const result = await user.save();
+    res.send({ 
+      success: true,
+      user: result 
+    });
+  } catch(error) {
+    res.status(400).send({
+      success: false,
+      error: error.message
+    });
+  }
+});
 module.exports = router;
